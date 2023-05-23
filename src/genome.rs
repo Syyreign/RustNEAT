63388d4bb1ceab8c6b3d178f9249042b86cf9606
Syyreign <spencerehart@hotmail.com>
2023-05-22 20:27:09 -0700

    Added more functions/tests
    Added tests, and add_nodes(). Added the sigmoid function to start evualating the network.

--- src/genome.rs
+++ src/genome.rs
@@ -1,5 +1,5 @@
 
-use std::collections::HashMap;
+use std::{collections::HashMap, f32::consts::E};
 
 enum GeneType{
     Input,
@@ -39,12 +39,17 @@ impl Connection{
             innovation_number: innovation_num, 
         }
     }
+
+    pub fn is_enabled(&self) -> bool {
+        self.is_enabled
+    }
 }
 
 pub struct Genome{
     connections: HashMap<(u32, u32), Connection>,
     node_genes: HashMap<u32, Gene>,
-    node_number: u32,
+    current_node_number: u32,
+    current_innovation_number: u32,
 
 }
 
@@ -63,34 +68,64 @@ impl Genome{
         Genome{
             connections: HashMap::new(),
             node_genes: h_genes,
-            node_number: in_nodes+out_nodes,
+            current_node_number: in_nodes+out_nodes,
+            current_innovation_number: 0,
         }
     }
 
-    pub fn add_node(&mut self, in_gene_num: u32, out_gene_num: u32, weight: f32, innovation_num: u32){
+    pub fn add_node(&mut self, in_gene_num: u32, out_gene_num: u32, weight: f32){
         if !Genome::does_connection_exist(&self.connections, in_gene_num, out_gene_num){
             return;
         }
 
-        self.node_genes.insert(self.node_number, Gene::new(self.node_number, GeneType::Hidden));
-        self.node_number += 1;
+        self.node_genes.insert(self.current_node_number, Gene::new(self.current_node_number, GeneType::Hidden));
+        let current_node_number = self.current_node_number;
+        self.current_node_number += 1;
+
+        // Disable the older connection
+        Genome::disable_connection(&mut self.connections, in_gene_num, out_gene_num);
+
+        // Add two new connections
+        let mut innov_num = self.get_next_innovation_number();
+        self.add_connection(in_gene_num, current_node_number, weight);
+        innov_num = self.get_next_innovation_number();
+        self.add_connection(current_node_number, out_gene_num, weight);
     }
 
-    pub fn add_connection(&mut self, in_gene_num: u32, out_gene_num: u32, weight: f32, innovation_num: u32) -> bool{
+    pub fn add_connection(&mut self, in_gene_num: u32, out_gene_num: u32, weight: f32) -> bool{
 
         if !self.node_genes.contains_key(&in_gene_num) ||
         !self.node_genes.contains_key(&out_gene_num){
             return false;
         }
 
-        // if Genome::should_disable_connection(&self.connections, in_gene_num, out_gene_num){
-        //     self.connections.entry((in_gene_num, out_gene_num)).and_modify(|k| k.is_enabled = false);
-        // }
-
+        let innov_num = self.get_next_innovation_number();
         self.connections.insert((in_gene_num, out_gene_num),
-         Connection::new(in_gene_num, out_gene_num, weight, true, innovation_num));
+         Connection::new(in_gene_num, out_gene_num, weight, true, innov_num));
 
         true
+    }
+
+    fn get_next_innovation_number(&mut self) -> u32{
+        let num = self.current_innovation_number;
+        self.current_innovation_number += 1;
+        num 
+    }
+
+    pub fn get_connection(&self, in_gene_num: u32, out_gene_num: u32) -> Option<&Connection>{
+        if !Genome::does_connection_exist(&self.connections, in_gene_num, out_gene_num) {
+            return None;
+        }
+
+        self.connections.get(&(in_gene_num, out_gene_num))
+    }
+
+    pub fn is_connection_enabled(&self, in_gene_num: u32, out_gene_num: u32) -> bool{
+        if !Genome::does_connection_exist(&self.connections, in_gene_num, out_gene_num) {
+            return false;
+        }
+
+        self.connections.get(&(in_gene_num, out_gene_num)).unwrap().is_enabled()
     }
 
     fn does_connection_exist(connections: &HashMap<(u32, u32), Connection>, in_gene_num: u32, out_gene_num: u32) -> bool{
@@ -100,4 +135,8 @@ impl Genome{
     fn disable_connection(connections: &mut HashMap<(u32, u32), Connection>, in_gene_num: u32, out_gene_num: u32){
         connections.entry((in_gene_num, out_gene_num)).and_modify(|k| k.is_enabled = false);
     }
+}
+
+pub fn sigmoid(input: f32) -> f32{
+    1.0 / (1.0 + E.powf( -4.9 * input))
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,49 @@ fn main() {
     println!("Hello, world!");
 
     let mut genome = genome::Genome::new(2,2);
-    genome.add_connection(1, 2, 0.5, 1);
+    genome.add_connection(1, 2, 0.5);
+    genome.add_node(1, 2, 0.5);
+    genome.add_node(1, 4, 0.5);
     let x = 5;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::genome::Genome;  
+
+    #[test]
+    fn test_add_first_connection() {
+        let mut genome = Genome::new(2,2);
+        genome.add_connection(1, 2, 0.5);
+        let is_enabled = genome.is_connection_enabled(1,2);
+        assert_eq!(is_enabled, true);
+    }
+
+    #[test]
+    fn test_add_hidden_node() {
+        let mut genome = Genome::new(2,2);
+        genome.add_connection(1, 2, 0.5);
+        genome.add_node(1, 2, 0.5);
+        let should_be_disabled = genome.is_connection_enabled(1,2);
+        assert_eq!(should_be_disabled, false);
+        let should_be_enabled = genome.is_connection_enabled(1, 4);
+        assert_eq!(should_be_enabled, true);
+    }
+
+    #[test]
+    fn test_add_two_hidden_nodes() {
+        let mut genome = Genome::new(2,2);
+        genome.add_connection(1, 2, 0.5);
+        genome.add_node(1, 2, 0.5);
+        genome.add_node(1, 4, 0.5);
+
+        let should_be_disabled_1 = genome.is_connection_enabled(1,2);
+        assert_eq!(should_be_disabled_1, false);
+
+        let should_be_disabled_2 = genome.is_connection_enabled(1, 4);
+        assert_eq!(should_be_disabled_2, false);
+
+        let should_be_enabled = genome.is_connection_enabled(1, 5);
+        assert_eq!(should_be_enabled, true);
+    }
 }
